9f8cddaa3dbf652c2208a6e2f844aa5f56cc2622
Ozkan Cimenli <ozkan.cimenli@hotmail.com>
2025-10-02 09:41:30 -0400

    nexr

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,21 @@ from . import models
 from .blueprints.members import members_bp
 from .blueprints.books import books_bp
 from .blueprints.loans import loans_bp
+from .blueprints.items import items_bp
+from .blueprints.orders import orders_bp
 from config import config
+from flask_swagger_ui import get_swaggerui_blueprint
+
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.yaml'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={'app_name': "Your API's Name"}
+)
+
 
 def create_app(config_name="development"):
     app = Flask(__name__)
@@ -18,8 +32,11 @@ def create_app(config_name="development"):
 
     # register blueprints
     app.register_blueprint(members_bp, url_prefix="/members")
-    app.register_blueprint(books_bp,   url_prefix="/books")
-    app.register_blueprint(loans_bp,   url_prefix="/loans")
+    app.register_blueprint(books_bp, url_prefix="/books")
+    app.register_blueprint(loans_bp, url_prefix="/loans")
+    app.register_blueprint(items_bp, url_prefix="/items")
+    app.register_blueprint(orders_bp, url_prefix="/orders")
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
     @app.get("/health")
     def health():
